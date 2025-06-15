ef5d5b012207d9b86f91085b042590ca694f43eb
trang.nguyen <maitrangnt2210@gmail.com>
2025-06-15 20:43:41 +0700

    chore: remove comp timeline

--- eStore/src/app/app.routes.ts
+++ eStore/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { TimelineComponent } from './timeline/timeline.component';
 
-export const routes: Routes = [
-    {path: 'timeline', component: TimelineComponent}
-];
+export const routes: Routes = [];
